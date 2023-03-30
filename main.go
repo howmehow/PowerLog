3ae50b5c14e3990371cfa923cb9547aab48d390a
michalwlodarski <howmehow@gmail.com>
2023-03-30 18:13:56 +0200

    Initial commit

--- main.go
+++ main.go
@@ -3,25 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
-package main
-
-import (
-"bufio"
-"fmt"
-"os"
-)
-
 func main() {
-	file, err := os.Open("file.txt") // For read access.
+	habits, err := os.Open("habits.txt")
 	if err != nil {
-		file, err = os.Create("file.txt")
+		habits, err = os.Create("habits.txt")
 		if err != nil {
 			fmt.Println(err)
 		}
 	}
+	//overview, err := os.Open("overview.txt")
+	//if err != nil {
+	//	overview, err = os.Create("overview.txt")
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//}
+	//stats, err := os.Open("stats.txt")
+	//if err != nil {
+	//	stats, err = os.Create("stats.txt")
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//}
+
+	data := make([]byte, 100)
+	count, err := habits.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -30,3 +44,6 @@ func main() {
 
 }
 
+// TODO So what I have to do step by step?
+// TODO Make sure there is three files in the folder
+// TODO at some point we gonna have to have them in specific folder - we gonna have to set the path.
